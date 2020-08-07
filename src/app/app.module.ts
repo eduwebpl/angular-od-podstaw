e8d916fd94d46dbf7ee3f07c52c605cf346ab278
Mateusz Kulesza <mateusz@eduweb.pl>
2020-08-07 16:09:58 +0200

    2.3. Tworzymy pod-komponenty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, WelcomeComponent]
 })
 export class AppModule { }
