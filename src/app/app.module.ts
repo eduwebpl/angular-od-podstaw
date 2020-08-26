c38540f7487f2f06451b84967f81c1ab8dd83392
Mateusz Kulesza <mateusz@eduweb.pl>
2020-08-26 19:06:02 +0200

    3.2. Organizacja i role komponentów

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HerobarComponent } from './components/herobar/herobar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    AboutComponent,
+    NavbarComponent,
+    HerobarComponent
   ],
   imports: [
     BrowserModule,
