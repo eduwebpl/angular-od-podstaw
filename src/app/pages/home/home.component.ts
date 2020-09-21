aa6f07ca6a983de14d4d90a7d5bdc14e1529c94a
Mateusz Kulesza <mateusz@eduweb.pl>
2020-09-21 22:57:38 +0200

    3.7. Wyświetlanie dynamicznych kolekcji - finish

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  recentProjects = [
+    {
+      id:123,
+      name: 'Project ABC',
+      image: 'http://lorempixel.com/400/400/technics/1/'
+    },
+    {
+      id:234,
+      name: 'Project XYZ',
+      image: 'http://lorempixel.com/400/400/technics/2/'
+    },
+    {
+      id:345,
+      name: 'Project QWERTY',
+      image: 'http://lorempixel.com/400/400/technics/3/'
+    },
+  ]
+
   readmore = false
 
   currentTab = ''
