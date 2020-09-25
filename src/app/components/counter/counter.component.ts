41ee2ab8e04c3782c982a2368758af38f92664b2
Mateusz Kulesza <mateusz@eduweb.pl>
2020-09-25 18:32:38 +0200

    3.10. Optymalizacja wykrywania zmian - finish

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,16 +1,19 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, ElementRef, Input, NgZone, OnInit, ViewChild } from '@angular/core';
 
 import * as Odometer from 'odometer'
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.scss']
+  styleUrls: ['./counter.component.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent implements OnInit, DoCheck {
 
   @Input()
   value = 0;
+
+  currentValue = 0;
 
   @Input()
   label = ''
@@ -18,19 +21,64 @@ export class CounterComponent implements OnInit {
   @ViewChild('counterElemRef', { read: ElementRef })
   counterElemRef: ElementRef
 
-  constructor() { }
+  @Input()
+  grow = 0
 
-  ngAfterViewInit(){
-    
-    const odometer = new Odometer({
-      el: this.counterElemRef.nativeElement
-    })
-
-    odometer.update(this.value)
-
-  }
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private zone: NgZone) { }
 
   ngOnInit(): void {
+    this.currentValue = this.value
+  }
+
+  updateAnimation = false
+
+  ngAfterViewInit() {
+    let odometer
+    this.zone.runOutsideAngular(() => {
+      odometer = new Odometer({
+        el: this.counterElemRef.nativeElement,
+      })
+
+      odometer.update(this.currentValue)
+    })
+
+    // setInterval(()=>{
+    //   this.value += this.grow
+    //   // this.cdr.detectChanges()
+    //   // this.cdr.markForCheck()
+    //   // this.counterElemRef.nativeElement.innerText = this.value
+    // },1000)
+
+    if (this.grow) {
+      this.zone.runOutsideAngular(() => {
+        setInterval(() => {
+          // Grow counter
+          this.currentValue += this.grow
+          odometer.update(this.currentValue)
+
+          this.zone.run(() => {
+
+            // Add pulse animation and remove after 1s
+            this.updateAnimation = true
+            setTimeout(() => { this.updateAnimation = false }, 1000)
+
+          })
+        },
+          // Update at random between 3s and 8s
+          3000 + (Math.random() * 5000)
+        )
+      })
+    }
+  }
+
+  checkIfUpdating() {
+    console.log('Detecting child changes')
+  }
+
+  ngDoCheck() {
+    console.log('Detecting parent changes')
   }
 
 }
