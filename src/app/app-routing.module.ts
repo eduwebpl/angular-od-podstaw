9f14755e568dec1ba3ceb7a4faed276b69fd57ac
Mateusz Kulesza <mateusz@eduweb.pl>
2020-10-27 12:41:44 +0100

    3.17. Moduł klienta HTTP i wysyłanie zapytań - start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,7 +42,8 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     // useHash: true,
-    anchorScrolling: 'enabled'
+    anchorScrolling: 'enabled',
+    scrollPositionRestoration:'top'
   })],
   exports: [RouterModule]
 })
